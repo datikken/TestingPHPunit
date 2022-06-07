6a7da65c7c3c5fd68307468f3a41e33b18737203
nshadybekov <shadybekov@cointelegraph.com>
2022-06-07 13:11:06 +0300

    initial

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testThatWorks()
+    {
+        $this->assertTrue(false);
+    }
+}
