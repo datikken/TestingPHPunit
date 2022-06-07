4b7613b225a6f05bd951580d8b507e585227900d
nshadybekov <shadybekov@cointelegraph.com>
2022-06-07 16:04:20 +0300

    php unit methods

--- tests/AppBundle/Factory/DinosaurFactoryTest.php
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -8,14 +8,35 @@ use AppBundle\Entity\Dinosaur;
 
 class DinosaurFactoryTest extends TestCase
 {
+    private $factory;
+
+    public function setUp(): void
+    {
+        $this->factory = new DinosaurFactory();
+    }
+
     public function testItGrowsAVelociraptor()
     {
-        $factory = new DinosaurFactory();
-        $dino = $factory->growVelociraptor(5);
+        $dino = $this->factory->growVelociraptor(5);
 
         $this->assertInstanceOf(Dinosaur::class, $dino);
         $this->assertIsString($dino->getGenus());
         $this->assertSame('Velociraptor', $dino->getGenus());
         $this->assertSame(5, $dino->getLength());
+    }
+
+    public function testitGrowsATriceratops()
+    {
+        $this->markTestIncomplete('Waiting for information');
+    }
+
+    public function testItGrowsABabyVelociraptor()
+    {
+        if (!class_exists('Nanny')) {
+            $this->markTestSkipped('There is no one to watch');
+        }
+
+        $dino = $this->factory->growVelociraptor(1);
+        $this->assertSame(1, $dino->getLength());
     }
 }
