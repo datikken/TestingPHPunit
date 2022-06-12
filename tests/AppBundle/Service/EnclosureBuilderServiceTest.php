a50e483d1d357d0e63b251468c2415d75fa43df7
nshadybekov <shadybekov@cointelegraph.com>
2022-06-12 19:51:02 +0300

    test entity flushed

--- tests/AppBundle/Service/EnclosureBuilderServiceTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\AppBundle\Service;
 
+use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
 use AppBundle\Service\EnclosureBuilderService;
 use Doctrine\ORM\EntityManagerInterface;
 use AppBundle\Factory\DinosaurFactory;
@@ -12,10 +14,19 @@ class EnclosureBuilderServiceTest extends TestCase
     public function testItBuildsAndPersistsEnclosure()
     {
         $em = $this->createMock(EntityManagerInterface::class);
+
+        $em->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(Enclosure::class));
+        $em
+            ->expects($this->atLeastOnce())
+            ->method('flush');
+
         $dinoFactory = $this->createMock(DinosaurFactory::class);
 
         $dinoFactory->expects($this->exactly(2))
             ->method('growFromSpecification')
+            ->willReturn(new Dinosaur())
             ->with($this->isType('string'));
 
         $builder = new EnclosureBuilderService($em, $dinoFactory);
